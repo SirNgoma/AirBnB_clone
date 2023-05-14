ff9d40f0c385162b6eb31e264ab1f945eb58411e
Themba <seconds.seconds1@gmail.com>
2023-05-14 11:14:24 -0700

    2

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,21 @@ class BaseModel:
     Base model class for all classes.
     """
 
-    def __init__(self):
+    def __init__(self, *args,**kwargs):
         """initialize of the class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key,value)
+            self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """str to repres class"""
